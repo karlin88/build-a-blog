bd4600bff68514606490affc7f3cc7d3ac005a98
Linda Pei <linda.pei@gmail.com>
2017-06-24 07:43:21 -0700

    skeleton is up and running, tested with one manual blog entry

--- main.py
+++ main.py
@@ -1,42 +1,51 @@
-from flask import Flask, request, url_for,render_template
+from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+import cgi
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Test1234!@localhost:8889/build-a-blog'
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:MyNewPass@localhost:8889/build-a-blog'
+#'mysql+pymysql://build-a-blog:blogger@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
 
 class Blog(db.Model):
-    blog_id = db.Column(db.Integer, primary_key = True, unique = True)
-    blog_name = db.Column(db.String(120))
-    extracted_text = db.Column(db.text)
-    image_path = db.Column(db.string(500))
+    id = db.Column(db.Integer, primary_key = True)
+    blogtitle = db.Column(db.String(120))
+    body = db.Column(db.Text)
+    imagepath = db.Column(db.String(120))
 
-    def __init__(self, blog_name, extracted_text = '',image_path = ''):
-        self.blog_name = blog_name
-        self.extracted_text = extracted_text
-        self.image_path = image_path
+    def __init__(self, blogtitle, body, imagepath):
+        self.blogtitle = blogtitle
+        self.body = body
+        self.imagepath = imagepath
 
-class BlogPostHashTags(db.Model):
-    hash_id = db.Column(db.Integer, db.ForeignKey('HashTags.hash_id'))
-    blog_id = db.Column(db.Integer, db.ForeignKey('Blog.blog_id'))
+    def __repr__(self):
+        return '<Blog %r>' % self.blogtitle
 
-    def __init__(self):
+def get_blogs():
+    return Blog.query.all()
 
-
-class HashTags(db.Model):
-    hash_id = db.Column(db.Integer, primary_key = True, unique = True)
+'''class HashTags(db.Model):
+    id = db.Column(db.Integer, primary_key = True, unique = True)
     hash_name = db.Column(db.String(120))
 
     def __init__(self,hash_name):
         self.hash_name = hash_name
 
+class BlogPostHashTags(db.Model):
+    hash_id = db.Column(db.Integer, db.ForeignKey('HashTags.id'))
+    blog_id = db.Column(db.Integer, db.ForeignKey('Blog.id'))
+    
+    def __init__(self,hash_id,blog_id):
+        self.hash_id = hash_id
+        self.blog_id = blog_id'''
+
 
 @app.route("/")
 def index():
-    return render_template('boilerplate.html')
+    return render_template('blog.html', blog = get_blogs())
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
